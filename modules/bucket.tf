a4d08ce3c3d310ab34ce47921b9d640be283f39a
Dimpulsunayana <121880181+Dimpulsunayana@users.noreply.github.com>
2023-03-14 19:12:36 +0530

    Update bucket.tf

--- modules/bucket.tf
+++ modules/bucket.tf
@@ -8,23 +8,23 @@
 # }
 
 resource "google_compute_instance" "vm_instance1" {
-#       name         = var.vm_name
-#       machine_type = var.mc_type
-#       zone         = var.zone
-#       boot_disk {
-#         initialize_params {
-#           image = var.image
-#         }
-#       }    
-#       network_interface {      
-#         #network = "dimpulsun"
-#         #allow_stopping_for_update = true 
-#         network =var.network
-#         subnetwork= var.subnetwork
-#         #desired_status = "TERMINATED"
-#         #allow_stopping_for_update = true 
-#         #subnetwork ="sunayana"
-#         access_config {
-#         }
-#       }
-#     }
+      name         = var.vm_name
+      machine_type = var.mc_type
+      zone         = var.zone
+      boot_disk {
+        initialize_params {
+          image = var.image
+        }
+      }    
+      network_interface {      
+        #network = "dimpulsun"
+        #allow_stopping_for_update = true 
+        network =var.network
+        subnetwork= var.subnetwork
+        #desired_status = "TERMINATED"
+        #allow_stopping_for_update = true 
+        #subnetwork ="sunayana"
+        access_config {
+        }
+      }
+    }
