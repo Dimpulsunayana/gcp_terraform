30bcbba37c3c2da4907e797df88f53091c89b2d8
Dimpulsunayana <121880181+Dimpulsunayana@users.noreply.github.com>
2023-03-14 19:13:40 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,9 +1,34 @@
-variable "bucket_name" {
-  description = "GCS bucket name"
-  type = string
+# variable "bucket_name" {
+#   description = "GCS bucket name"
+#   type = string
+# }
+
+# variable "bucket_loc" {
+#   description = "GCS bucket location"
+#   type = string
+# }
+
+variable "vm_name" {
+  description = "GCP project ID"
+  default = "dimpultest35r875785"
 }
 
-variable "bucket_loc" {
-  description = "GCS bucket location"
-  type = string
+variable "mc_type" {
+  description = "GCP project ID"
+  #default = "f1-micro"
+}
+
+variable "image" {
+  description = "GCP project ID"
+  default = "debian-cloud/debian-9"
+}
+
+variable "network" {
+  description = "GCP project ID"
+  default = "acn-cio-project-vpc"
+}
+
+variable "subnetwork" {
+  description = "GCP project ID"
+  default = "us-central1-public-subnet"
 }
